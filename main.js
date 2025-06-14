60d81d0d6ea0f24ae912064afe37143080ab3bbb
Taimoor Ali <taimoorali202@gmail.com>
2025-06-14 23:29:25 +0500

    refactor the logic for setting rgb values

--- main.js
+++ main.js
@@ -4,12 +4,42 @@ function createGrid(size) {
   for (let i = 1; i <= size * size; i++) {
     const div = document.createElement('div');
     div.style.width = `${100/size}%`;
-    div.addEventListener('mouseenter', () => div.style.backgroundColor = 'red');
+    div.addEventListener('mouseenter', assignRGBValue);
     container.appendChild(div);
   }
 }
 
 createGrid(16);
+
+function randomRGBValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function getPreviousRBGValues(e) {
+  const rgba = e.target.style.backgroundColor;
+  return rgba.slice(5, -1).split(', ').map(v => +v.trim());
+}
+
+function assignRGBValue(e) {
+  let r;
+  let g;
+  let b;
+  let a;
+
+  if (e.target.style.backgroundColor) {
+    [r, g, b, a] = getPreviousRBGValues(e);
+    if (a < 1.0) {
+      a += 0.1;
+    }
+  } else {
+    r = randomRGBValue();
+    g = randomRGBValue();
+    b = randomRGBValue();
+    a = 0.1;
+  }
+
+  e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${a})`;
+}
 
 const gridSizeBtn = document.querySelector('#btn-div button');
 
