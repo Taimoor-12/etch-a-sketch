6cc29b3de62713b8b5fc133fc493a8d9d71555de
Taimoor Ali <taimoorali202@gmail.com>
2025-06-14 21:22:56 +0500

    add an event listener to each child div

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function createGrid(size) {
   for (let i = 1; i <= size * size; i++) {
     const div = document.createElement('div');
     div.style.width = `${100/size}%`;
+    div.addEventListener('mouseenter', () => div.style.backgroundColor = 'red');
     container.appendChild(div);
   }
 }
